5c57e1dd24827f3c050234e733146b684b0e1675
MrNonsense <trialess@hotmail.it>
2016-12-07 10:44:00 +0100

    deleted commented code commit test

--- app/src/main/java/com/atrianni/mathstuff/MainActivity.java
+++ app/src/main/java/com/atrianni/mathstuff/MainActivity.java
@@ -26,15 +26,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         totalTextView = (TextView) findViewById(R.id.totalTextView);
         ofTextView = (TextView) findViewById(R.id.ofTextView);
         whatTextView = (TextView) findViewById(R.id.whatTextView);
